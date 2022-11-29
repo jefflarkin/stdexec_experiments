b2971640c1c48321bb7e242c1c7568245c38ca63
Jeff Larkin <jefflarkin@gmail.com>
2022-11-29 16:32:26 -0500

    Cleaner style on hello

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -7,9 +7,8 @@ int main()
   exec::static_thread_pool ctx(1);
   auto sch = ctx.get_scheduler();
 
-  auto hello = stdexec::then(stdexec::schedule(sch), 
-                 []{ printf("Hello, World!\n"); }
-               )
+  auto hello = stdexec::schedule(sch)
+             | stdexec::then([] { printf("Hello, World!\n"); })
              | stdexec::then([] { printf("Good Bye, Cruel World!\n");});
   
   stdexec::sync_wait(hello);
